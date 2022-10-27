9b468d61c86be45c36e0a685f89c5dd54edfc355
thienle77 <tle9677@gmail.com>
2022-10-26 23:47:41 -0400

    Test passed

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -28,6 +28,24 @@ public class LogginLabTest {
             } else {
                 logger.log(Level.INFO, "Threshold finally reached!");
                 assertFalse(lab.thresholdExceeds(i));
+
+                }
+            }
+        }
+    @org.junit.Test
+    public void thresholdReached() {
+        Integer finalLimit = 5;
+
+        LogginLab lab = new LogginLab();
+        lab.thresholdReached(4);
+
+        for (Integer i = 1; i < finalLimit; i++) {
+            if (lab.thresholdReached(i)) {
+                logger.log(Level.INFO, "Threshold reached");
+                assertTrue(lab.thresholdReached(i));
+            } else {
+                logger.log(Level.INFO,"Threshold not reached" );
+                assertFalse(lab.thresholdReached(i));
             }
         }
     }
